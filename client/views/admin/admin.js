2c25ed5583eea7b0287b97de86455c469f765e48
Nick <nick.bucheleres@gmail.com>
2015-04-24 14:17:54 -0700

    admin portal displays whether or not user is admin; next need to make admin able to grant admin status to non-admin users

--- client/views/admin/admin.js
+++ client/views/admin/admin.js
@@ -11,6 +11,13 @@ Router.route('admin', {
 Template.adminTemplate.helpers({
 	users: function(){
 		return Meteor.users.find();
+	},
+
+	notAdmin: function(){
+		var user_object = Meteor.users.find({_id: this._id}, {admin: 1}).fetch();
+		var admin_status = user_object[0].admin;
+		// console.log(admin_status);
+		return admin_status != 'true';
 	}
 });
 
--- server/users.js
+++ server/users.js
@@ -4,9 +4,10 @@ Meteor.publish('users', function(){
 	if(!this.userId){
 		console.log('you are not signed in');
 	}
-	else if(this.userId != '4zekmrHHiDGDuBNwZ' ){
+	else if(this.userId != 'GfjaBg9ghfGefk9F8' ){
 		console.log('you are not an admin');
 	} else {
-		return Meteor.users.find();
+		return Meteor.users.find({}, {fields: {createdAt: 1, admin: 1, emails: 1}}); 
 	}
-})
+});
+
